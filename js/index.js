f2652d80f143699b48bd35f1c4977f989de51d05
Sinan-Kar <sinankara.sinankara@gmail.com>
2021-03-14 09:25:09 +0300

    starbucks-sablonu

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+  document.querySelector('.starbucks').src = anything;
+}
+function changeCricleColor(color){
+  const circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
